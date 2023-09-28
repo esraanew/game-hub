22fc7ec1f583b8f4f261cabb50df68fa05e61133
Esraa Samir <esamir@git01.gn4me>
2023-09-28 21:36:29 +0300

    Show loading skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,23 @@
 import { SimpleGrid, Text } from '@chakra-ui/react'
 import useGames from '../hooks/useGames'
 import GameCard from './GameCard'
+import GameCardSkelton from './GameCardSkelton'
 
 const GameGrid = () => {
-  const { games, error } = useGames()
+  const { games, error, isLoading } = useGames()
+  const skeltonData = [1, 2, 3, 4, 5, 6, 7, 8]
   return (
     <>
       {error && <Text color='red'>{error}</Text>}
-      <SimpleGrid columns={{sm:1,md:2,lg:3,xl:4}} spacing={10} padding='10px'>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={10}
+        padding='10px'
+      >
+        {isLoading &&
+          skeltonData.map(skeleton => {
+            return <GameCardSkelton key={skeleton} />
+          })}
         {games.map(game => {
           return <GameCard key={game.id} game={game} />
         })}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,17 +2,17 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../Services/api-client";
 
-export interface Platform{
-  id:number;
-  name:string;
-  slug:string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 export interface Game {
   id: number;
   name: string;
-  background_image:string;
-  parent_platforms:{platform:Platform}[];
-  metacritic:number;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 interface FetchGamesResponse {
   count: number;
@@ -22,17 +22,22 @@ const useGames = () => {
   const controller = new AbortController();
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
     // return () => controller.abort();
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
