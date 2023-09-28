4a3a2ca3d6c37e317dfb93613c98627a2442643e
Esraa Samir <esamir@git01.gn4me>
2023-09-28 14:28:18 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-
-function App() {
-
+import { Grid, GridItem, Show } from '@chakra-ui/react'
+function App () {
   return (
-    <div className="App">
-      <Button colorScheme='blue'>Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav " "aside main"`
+      }}
+    >
+      <GridItem area='nav' bg='coral'>
+        nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='gold'>
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area='main' bg='dodgerblue'>
+        Main
+      </GridItem>
+    </Grid>
   )
 }
 
