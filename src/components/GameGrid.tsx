5880cb3155f1ad19d26bf48d38c3510e6e8c0864
Esraa Samir <esamir@git01.gn4me>
2023-09-28 17:20:19 +0300

    Create a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,24 +1,9 @@
-import { Grid, GridItem, Text } from '@chakra-ui/react'
-import apiClient from '../Services/api-client'
-import { useEffect, useState } from 'react'
-interface Game {
-  id: number
-  name: string
-}
-interface FetchGamesResponse {
-  count: number
-  results: Game[]
-}
-const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([])
-  const [error, setError] = useState('')
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>('/games')
-      .then(res => setGames(res.data.results))
-      .catch(err => setError(err.message))
-  }, [])
+import { Text } from '@chakra-ui/react'
+import useGames from '../hooks/useGames'
 
+const GameGrid = () => {
+const {games,error} = useGames();
+console.log(games)
   return (
     <>
     {error && <Text color="red">{error}</Text>}
